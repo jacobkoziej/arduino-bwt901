e19f1b485d2584d26b7a88425eb58d981f2c62af
Jacob Koziej <jacobkoziej@gmail.com>
2021-04-23 00:26:22 -0400

    Create BWT901 interface library

---
+++ src/bwt901.h
@@ -0,0 +1,23 @@
+/*
+ * arduino-bwt901.h -- BWT901 interface library
+ * Copyright (C) 2021  Jacob Koziej <jacobkoziej@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef _BTW901_H
+#define _BTW901_H
+
+
+#endif /* _BTW901_H */
