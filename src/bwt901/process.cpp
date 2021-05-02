7b7c775a83acc1d9e8e27add0286688612f88fbc
Jacob Koziej <jacobkoziej@gmail.com>
2021-05-02 14:31:19 -0400

    Define BWT901_process::deserialize()

---
+++ src/bwt901/process.cpp
@@ -0,0 +1,34 @@
+/*
+ * process.cpp -- BWT901 data processing
+ * Copyright (C) 2021  Jacob Koziej <jacobkoziej@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "process.h"
+
+#include <Arduino.h>
+
+
+inline int16_t BWT901_process::deserialize(uint8_t in[2])
+{
+	int16_t tmp;
+
+	// low byte is always first
+	tmp   = in[0];
+	tmp <<= 8;
+	tmp  |= in[1];
+
+	return tmp;
+}
---
+++ src/bwt901/process.h
@@ -0,0 +1,31 @@
+/*
+ * process.h -- BWT901 data processing
+ * Copyright (C) 2021  Jacob Koziej <jacobkoziej@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef _BWT901_PROCESS_H
+#define _BWT901_PROCESS_H
+
+#include <Arduino.h>
+
+
+class BWT901_process {
+	public:
+		inline int16_t deserialize(uint8_t in[2]);
+};
+
+
+#endif /* _BWT901_PROCESS_H */
